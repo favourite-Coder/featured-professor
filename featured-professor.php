d03709787bd4291ad4493d271557568afbf4738e
Favouritecoder <favourgabrielcutie08@gmail.com>
2024-01-22 06:04:35 +0100

    Adding Related Posts to Professor Detail Page

--- featured-professor.php
+++ featured-professor.php
@@ -10,11 +10,20 @@
 if( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 require_once plugin_dir_path(__FILE__) . 'inc/generateProfessorHTML.php';
+require_once plugin_dir_path(__FILE__) . 'inc/relatedPostsHTML.php';
 
 class FeaturedProfessor {
   function __construct() {
     add_action('init', [$this, 'onInit']);
     add_action('rest_api_init', [$this, 'profHTML']);
+    add_filter('the_content', [$this, 'addRelatedPosts']);
+  }
+
+  function addRelatedPosts($content) {
+    if (is_singular('professor') && in_the_loop() && is_main_query()) {
+      return $content . relatedPostsHTML(get_the_id());
+    }
+    return $content;
   }
 
   function profHTML() {
